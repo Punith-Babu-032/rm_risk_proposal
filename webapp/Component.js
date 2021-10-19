6765225899348221c8e77005d722184461e7fea5
Punith Babu <punith.sapdeveloper@gmail.com>
2021-10-19 19:21:13 +0000

    Commit before Pull

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,16 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"./model/models",
-	"./controller/ErrorHandler"
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "./model/models",
+    "./controller/ErrorHandler"
 ], function (UIComponent, Device, models, ErrorHandler) {
-	"use strict";
+    "use strict";
 
-	return UIComponent.extend("gmriskproposal.Component", {
+    return UIComponent.extend("gmriskproposal.Component", {
 
-		metadata : {
-			manifest: "json"
-		},
+        metadata: {
+            manifest: "json"
+        },
 
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
@@ -18,22 +18,22 @@ sap.ui.define([
 		 * @public
 		 * @override
 		 */
-		init : function () {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
 
-			// initialize the error handler with the component
-			this._oErrorHandler = new ErrorHandler(this);
+            // initialize the error handler with the component
+            this._oErrorHandler = new ErrorHandler(this);
 
-			// set the device model
+            // set the device model
             this.setModel(models.createDeviceModel(), "device");
-            
+
             // Set Application Component Data
             this.setComponentData();
 
-			// create the views based on the url/hash
-			this.getRouter().initialize();
-		},
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+        },
 
 		/**
 		 * The component is destroyed by UI5 automatically.
@@ -41,11 +41,11 @@ sap.ui.define([
 		 * @public
 		 * @override
 		 */
-		destroy : function () {
-			this._oErrorHandler.destroy();
-			// call the base component's destroy function
-			UIComponent.prototype.destroy.apply(this, arguments);
-		},
+        destroy: function () {
+            this._oErrorHandler.destroy();
+            // call the base component's destroy function
+            UIComponent.prototype.destroy.apply(this, arguments);
+        },
 
 		/**
 		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
@@ -53,29 +53,38 @@ sap.ui.define([
 		 * @public
 		 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
 		 */
-		getContentDensityClass : function() {
-			if (this._sContentDensityClass === undefined) {
-				// check whether FLP has already set the content density class; do nothing in this case
-				// eslint-disable-next-line sap-no-proprietary-browser-api
-				if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
-					this._sContentDensityClass = "";
-				} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
-					this._sContentDensityClass = "sapUiSizeCompact";
-				} else {
-					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
-					this._sContentDensityClass = "sapUiSizeCozy";
-				}
-			}
-			return this._sContentDensityClass;
+        getContentDensityClass: function () {
+            if (this._sContentDensityClass === undefined) {
+                // check whether FLP has already set the content density class; do nothing in this case
+                // eslint-disable-next-line sap-no-proprietary-browser-api
+                if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
+                    this._sContentDensityClass = "";
+                } else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else {
+                    // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+            }
+            return this._sContentDensityClass;
         },
-        
-        setComponentData: function() {
+
+        setComponentData: function () {
+            var app;
+            if (this.getComponentData()) {
+                switch (this.getComponentData().CallingApp) {
+                    case "MyTask":
+                        app = "ApproveApp";
+                        break;
+                    default:
+                        app = "RequestApp";
+                        break;
+                }
+            }
             var appConfigData = {
-                app: "RequestApp"
+                app: app
             };
             this.setModel(new sap.ui.model.json.JSONModel(appConfigData), "appConfigModel")
         }
-
-	});
-
+    });
 });
