8ea04e783d919c2c73efa7575e9fd5c24f5f98ca
Mahesh Kumar Palavalli <p.maheshkumarreddy@gmail.com>
2021-10-20 08:32:08 +0000

    mahesh changes

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -1,72 +1,92 @@
 sap.ui.define([
-	"./BaseController",
-	"sap/ui/model/json/JSONModel",
-	"../model/formatter",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
-	"use strict";
+    "use strict";
 
-	return BaseController.extend("gmriskproposal.controller.Worklist", {
+    return BaseController.extend("gmriskproposal.controller.Worklist", {
 
-		formatter: formatter,
+        formatter: formatter,
 
-		/* =========================================================== */
-		/* lifecycle methods                                           */
-		/* =========================================================== */
+        /* =========================================================== */
+        /* lifecycle methods                                           */
+        /* =========================================================== */
 
 		/**
 		 * Called when the worklist controller is instantiated.
 		 * @public
 		 */
-		onInit : function () {
-			var oViewModel,
-				iOriginalBusyDelay;
+        onInit: function () {
+            var oViewModel,
+                iOriginalBusyDelay;
 
-			// Put down worklist table's original value for busy indicator delay,
-			// so it can be restored later on. Busy handling on the table is
-			// taken care of by the table itself.
-			// keeps the search state
-			this._aTableSearchState = [];
+            // Put down worklist table's original value for busy indicator delay,
+            // so it can be restored later on. Busy handling on the table is
+            // taken care of by the table itself.
+            // keeps the search state
+            this._aTableSearchState = [];
 
-			// Model used to manipulate control states
-			oViewModel = new JSONModel({
-				worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
-				shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
-				shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
-				shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
-				tableNoDataText : this.getResourceBundle().getText("tableNoDataText"),
-				tableBusyDelay : 0
-			});
-			this.setModel(oViewModel, "worklistView");
+            // Model used to manipulate control states
+            oViewModel = new JSONModel({
+                worklistTableTitle: this.getResourceBundle().getText("worklistTableTitle"),
+                shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
+                shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
+                shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
+                tableNoDataText: this.getResourceBundle().getText("tableNoDataText"),
+                tableBusyDelay: 0
+            });
+            this.setModel(oViewModel, "worklistView");
 
-			// Make sure, busy indication is showing immediately so there is no
-			// break after the busy indication for loading the view's meta data is
-			// ended (see promise 'oWhenMetadataIsLoaded' in AppController)
+            // Make sure, busy indication is showing immediately so there is no
+            // break after the busy indication for loading the view's meta data is
+            // ended (see promise 'oWhenMetadataIsLoaded' in AppController)
+            this.setModel(new JSONModel({
+                impacts: [],
+                causes: []
+            }), "saveModel");
 
-		},
+            this.setModel(new JSONModel({
+                impacts: [{
+                    impactId: "1",
+                    impactText: "Impact 1"
+                }, {
+                    impactId: "2",
+                    impactText: "Impact 2"
+                }],
+                causes: [{
+                    causeId: "1",
+                    causeText: "Cause 1"
+                }, {
+                    causeId: "2",
+                    causeText: "Cause 2"
+                }]
+            }), "dropdowns");
+        },
 
-		/* =========================================================== */
-		/* event handlers                                              */
-		/* =========================================================== */
-        
-         goToPrevPage: function() {
+        /* =========================================================== */
+        /* event handlers                                              */
+        /* =========================================================== */
+
+        goToPrevPage: function () {
             this.byId("proposeRiskWizard").previousStep();
             this.byId("prevPageAction").setVisible(false);
             this.byId("nextPageAction").setVisible(true);
         },
 
-        goToNextPage: function() {
+        goToNextPage: function () {
             this.byId("proposeRiskWizard").nextStep();
             this.byId("nextPageAction").setVisible(false);
             this.byId("prevPageAction").setVisible(true);
         },
 
-        handleStepActivate: function(oEvent) {
+        handleStepActivate: function (oEvent) {
 
         },
 
-        
+
 
 		/**
 		 * Triggered by the table's 'updateFinished' event: after new table
@@ -77,101 +97,151 @@ sap.ui.define([
 		 * @param {sap.ui.base.Event} oEvent the update finished event
 		 * @public
 		 */
-		onUpdateFinished : function (oEvent) {
-			// update the worklist's object counter after the table update
-			var sTitle,
-				oTable = oEvent.getSource(),
-				iTotalItems = oEvent.getParameter("total");
-			// only update the counter if the length is final and
-			// the table is not empty
-			if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
-				sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
-			} else {
-				sTitle = this.getResourceBundle().getText("worklistTableTitle");
-			}
-			this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
-		},
+        onUpdateFinished: function (oEvent) {
+            // update the worklist's object counter after the table update
+            var sTitle,
+                oTable = oEvent.getSource(),
+                iTotalItems = oEvent.getParameter("total");
+            // only update the counter if the length is final and
+            // the table is not empty
+            if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
+                sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
+            } else {
+                sTitle = this.getResourceBundle().getText("worklistTableTitle");
+            }
+            this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
+        },
 
 		/**
 		 * Event handler when a table item gets pressed
 		 * @param {sap.ui.base.Event} oEvent the table selectionChange event
 		 * @public
 		 */
-		onPress : function (oEvent) {
-			// The source is the list item that got pressed
-			this._showObject(oEvent.getSource());
-		},
+        onPress: function (oEvent) {
+            // The source is the list item that got pressed
+            this._showObject(oEvent.getSource());
+        },
 
 		/**
 		 * Event handler for navigating back.
 		 * We navigate back in the browser history
 		 * @public
 		 */
-		onNavBack : function() {
-			// eslint-disable-next-line sap-no-history-manipulation
-			history.go(-1);
-		},
+        onNavBack: function () {
+            // eslint-disable-next-line sap-no-history-manipulation
+            history.go(-1);
+        },
 
 
-		onSearch : function (oEvent) {
-			if (oEvent.getParameters().refreshButtonPressed) {
-				// Search field's 'refresh' button has been pressed.
-				// This is visible if you select any master list item.
-				// In this case no new search is triggered, we only
-				// refresh the list binding.
-				this.onRefresh();
-			} else {
-				var aTableSearchState = [];
-				var sQuery = oEvent.getParameter("query");
+        onSearch: function (oEvent) {
+            if (oEvent.getParameters().refreshButtonPressed) {
+                // Search field's 'refresh' button has been pressed.
+                // This is visible if you select any master list item.
+                // In this case no new search is triggered, we only
+                // refresh the list binding.
+                this.onRefresh();
+            } else {
+                var aTableSearchState = [];
+                var sQuery = oEvent.getParameter("query");
 
-				if (sQuery && sQuery.length > 0) {
-					aTableSearchState = [new Filter("CategoryID", FilterOperator.Contains, sQuery)];
-				}
-				this._applySearch(aTableSearchState);
-			}
+                if (sQuery && sQuery.length > 0) {
+                    aTableSearchState = [new Filter("CategoryID", FilterOperator.Contains, sQuery)];
+                }
+                this._applySearch(aTableSearchState);
+            }
 
-		},
+        },
 
 		/**
 		 * Event handler for refresh event. Keeps filter, sort
 		 * and group settings and refreshes the list binding.
 		 * @public
 		 */
-		onRefresh : function () {
-			var oTable = this.byId("table");
-			oTable.getBinding("items").refresh();
-		},
+        onRefresh: function () {
+            var oTable = this.byId("table");
+            oTable.getBinding("items").refresh();
+        },
 
-		/* =========================================================== */
-		/* internal methods                                            */
-		/* =========================================================== */
+        /* =========================================================== */
+        /* internal methods                                            */
+        /* =========================================================== */
 
+        handleAddCauses: function () {
+            // open popup
+            if (!this.causesPopup) {
+                this.causesPopup = new sap.m.SelectDialog({
+                    title: "Select a Cause",
+                    confirm: function (oEvent) {
+                        var aContexts = oEvent.getParameter("selectedContexts");
+                        var mObject = aContexts[0].getObject();
+                        var oModel = this.getView().getModel("saveModel");
+                        var aData = oModel.getData();
+                        aData.causes.push({ "causeText": mObject.causeText });
+                        oModel.setData(aData);
+                        oModel.refresh();
+                    }.bind(this)
+                });
+                this.causesPopup.bindAggregation("items", {
+                    path: "dropdowns>/causes",
+                    template: new sap.m.StandardListItem({
+                        title: "{dropdowns>causeText}"
+                    })
+                });
+                this.getView().addDependent(this.causesPopup);
+            }
+            this.causesPopup.open();
+        },
+
+        handleAddImpacts: function () {
+            if (!this.impactsPopup) {
+                this.impactsPopup = new sap.m.SelectDialog({
+                    title: "Select an Impact",
+                    confirm: function (oEvent) {
+                        var aContexts = oEvent.getParameter("selectedContexts");
+                        var mObject = aContexts[0].getObject();
+                        var oModel = this.getView().getModel("saveModel");
+                        var aData = oModel.getData();
+                        aData.causes.push({ "impactText": mObject.impactText });
+                        oModel.setData(aData);
+                        oModel.refresh();
+                    }.bind(this)
+                });
+                this.impactsPopup.bindAggregation("items", {
+                    path: "dropdowns>/impacts",
+                    template: new sap.m.StandardListItem({
+                        title: "{dropdowns>impactText}"
+                    })
+                });
+                this.getView().addDependent(this.impactsPopup);
+            }
+            this.impactsPopup.open();
+        },
 		/**
 		 * Shows the selected item on the object page
 		 * On phones a additional history entry is created
 		 * @param {sap.m.ObjectListItem} oItem selected Item
 		 * @private
 		 */
-		_showObject : function (oItem) {
-			this.getRouter().navTo("object", {
-				objectId: oItem.getBindingContext().getProperty("CategoryID")
-			});
-		},
+        _showObject: function (oItem) {
+            this.getRouter().navTo("object", {
+                objectId: oItem.getBindingContext().getProperty("CategoryID")
+            });
+        },
 
 		/**
 		 * Internal helper method to apply both filter and search state together on the list binding
 		 * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
 		 * @private
 		 */
-		_applySearch: function(aTableSearchState) {
-			var oTable = this.byId("table"),
-				oViewModel = this.getModel("worklistView");
-			oTable.getBinding("items").filter(aTableSearchState, "Application");
-			// changes the noDataText of the list in case there are no filter results
-			if (aTableSearchState.length !== 0) {
-				oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
-			}
-		}
+        _applySearch: function (aTableSearchState) {
+            var oTable = this.byId("table"),
+                oViewModel = this.getModel("worklistView");
+            oTable.getBinding("items").filter(aTableSearchState, "Application");
+            // changes the noDataText of the list in case there are no filter results
+            if (aTableSearchState.length !== 0) {
+                oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
+            }
+        }
 
-	});
+    });
 });
